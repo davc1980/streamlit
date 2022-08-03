242f1f35223065f28051fa0a40b2a02bf78ed9f3
davc1980 <50352946+davc1980@users.noreply.github.com>
2022-08-03 12:20:53 -0700

    Update testing_streamlit.py

--- testing_streamlit.py
+++ testing_streamlit.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 st.write("""my first App""")
 df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
+  'Numero': [1, 2, 3, 4],
+  'Calificacion': [10, 20, 30, 40]
 })
 
 df
